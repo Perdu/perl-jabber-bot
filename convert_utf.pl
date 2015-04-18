fbeeffb28b70ec1f4977a77fb02fb03c6c4f1be6
Célestin Matte <celestin.matte@gmail.com>
2015-04-18 12:47:21 +0200

    Fixed convert_utf8 script

--- convert_utf.pl
+++ convert_utf.pl
@@ -8,10 +8,10 @@ while (<>) {
 	my $line = $_;
 	my $line_converted = $line;
 	utf8::encode($line_converted);
-	if ($line =~ /[\x{c7}\x{e9}\x{e0}]/) {
+	# Ç, é, è, ï, â, ê, ç
+	if ($line =~ /[\x{c7}\x{e9}\x{e0}\x{ef}\x{e2}\x{e8}\x{e7}]/ && $line !~ /\x{e2}\x{80}/) {
 		print $line_converted;
 	} else {
 		print $line;
 	}
 }
-
