820388e2ee0df0d2e9c663a99e51fbe40f0409aa
Célestin Matte <celestin.matte@gmail.com>
2016-06-15 21:45:05 +0200

    mysql: set all charset to utf8

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS anu;
-CREATE DATABASE anu;
+CREATE DATABASE anu CHARACTER SET utf8 COLLATE utf8_general_ci;
 connect anu;
 
 -- contains a copy of all score at every timestamp (download)
@@ -10,18 +10,18 @@ CREATE TABLE `quotes` (
        quote varchar(10000) NOT NULL,
        `timestamp` timestamp DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`quote_id`)
-);
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE `words_in_quote` (
        quote_id int(3) NOT NULL,
        word varchar(50) NOT NULL,
        PRIMARY KEY (`quote_id`, `word`),
        FOREIGN KEY (`quote_id`) REFERENCES `quotes`(`quote_id`)
-);
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE `words_links` (
        word1 varchar(50) NOT NULL,
        word2 varchar(50) NOT NULL,
        occurences int(5) NOT NULL,
        PRIMARY KEY (`word1`, `word2`)
-);
+) DEFAULT CHARSET=utf8;
